a2106f1ceb7537534765153e57aa0461d143ab41
Shree3612 <shripadkulkarni3612@gmail.com>
2024-12-16 18:22:13 +0530

    RestTemplate Configuration

--- src/main/java/com/remoteapi/news/controller/NewsController.java
+++ src/main/java/com/remoteapi/news/controller/NewsController.java
@@ -2,14 +2,20 @@ package com.remoteapi.news.controller;
 
 import com.remoteapi.news.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/news")
 public class NewsController {
 
-        //controller class
         @Autowired
         NewsService newsService;
+
+        @GetMapping("/country")
+        public Object getNewsByCountry(@RequestParam("country") String country, @RequestParam("apiKey") String apiKey) {
+                return newsService.getNewsByCountry(country, apiKey);
+        }
 }
--- src/main/java/com/remoteapi/news/service/NewsService.java
+++ src/main/java/com/remoteapi/news/service/NewsService.java
@@ -2,7 +2,30 @@ package com.remoteapi.news.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NewsService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    //complete remote API : https://newsapi.org/v2/top-headlines?country=us&apiKey=251dcfe49d8844eab78ccfccaae3e5bf
+
+    String baseUrl = "https://newsapi.org/v2/top-headlines";
+
+
+    public Object getNewsByCountry(String country, String apiKey) {
+        String url = prepareUrl(String baseUrl, String country, String apiKey);
+        Object response = restTemplate.getForObject(url, Object.class);
+        return response;
+    }
+
+
+    public String prepareUrl(String baseUrl, String country, String apiKey) {
+        String completeUrl = baseUrl+"?country="+country+"&apiKey="+apiKey;
+        return completeUrl;
+    }
 }
